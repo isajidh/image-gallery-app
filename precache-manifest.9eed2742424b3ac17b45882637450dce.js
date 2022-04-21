1982e95a01f65b96a179b6e0c74382c32da37875
Ammar <a.b.ameerdeen@gmail.com>
2022-04-21 23:05:18 +0530

    Updates

---
+++ precache-manifest.9eed2742424b3ac17b45882637450dce.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "73f8a8745b9fdd82502b5c78790eb2d8",
+    "url": "/image-gallery-app/index.html"
+  },
+  {
+    "revision": "4a56214c59e60f4c6aaf",
+    "url": "/image-gallery-app/static/css/2.929389fa.chunk.css"
+  },
+  {
+    "revision": "706c7a950925fbebd7c6",
+    "url": "/image-gallery-app/static/css/main.334fbf4f.chunk.css"
+  },
+  {
+    "revision": "4a56214c59e60f4c6aaf",
+    "url": "/image-gallery-app/static/js/2.e8dce859.chunk.js"
+  },
+  {
+    "revision": "3a8144662ab8594515759b89e5c47d99",
+    "url": "/image-gallery-app/static/js/2.e8dce859.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "706c7a950925fbebd7c6",
+    "url": "/image-gallery-app/static/js/main.8f94a486.chunk.js"
+  },
+  {
+    "revision": "ec49d3e14fc8cb1fbab6",
+    "url": "/image-gallery-app/static/js/runtime-main.a5baf6c5.js"
+  }
+]);
